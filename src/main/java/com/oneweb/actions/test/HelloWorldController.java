980c3e63f30a291650013eed84315f511bd3b601
Gabriel Barbosa Gaspar Veloso <gabriel.veloso@celfocus.com>
2024-05-29 15:05:23 +0100

    feat(DPFH-2574): dummy changes to have a changelog

--- src/main/java/com/oneweb/actions/test/HelloWorldController.java
+++ src/main/java/com/oneweb/actions/test/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping(path="/")
     public String helloWorld() {
-        return "Hello, World!";
+        return "Hello, entire World!";
     }
 }
