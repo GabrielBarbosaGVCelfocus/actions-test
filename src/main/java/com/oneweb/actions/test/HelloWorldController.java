df06d5b7dcfa77c3cad4b52dc405e71c0dc0c458
Gabriel Barbosa Gaspar Veloso <gabriel.veloso@celfocus.com>
2024-05-29 15:56:50 +0100

    feat(DPFH-2574): adding dummy method

--- src/main/java/com/oneweb/actions/test/HelloWorldController.java
+++ src/main/java/com/oneweb/actions/test/HelloWorldController.java
@@ -14,4 +14,9 @@ public class HelloWorldController {
     public String more() {
         return "What more do you want?";
     }
+
+    @RequestMapping(path="/another-test")
+    public String anotherTest() {
+        return "Trying out even more stuff, this time with a PR.";
+    }
 }
